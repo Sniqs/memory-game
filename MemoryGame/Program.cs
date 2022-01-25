f2f1cc6024d6bd8041cb89116cd2513df659b532
Hubert Życiński <hzycinski@gmail.com>
2022-01-25 15:28:22 +0100

    Load words from file

--- MemoryGame/Program.cs
+++ MemoryGame/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MemoryGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words;
+            try
+            {
+                words = System.IO.File.ReadAllLines(@"Words.txt");
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine($"{e.Message}");
+                Console.WriteLine("Make sure that the Words.txt file is located in the directory given above.");
+                Console.ReadKey();
+                return;
+            }
+
+            
+
+        }
+    }
+}
