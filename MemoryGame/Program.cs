b8ab2de4f698012552616c6c4e100b4f1080d3d1
Hubert Życiński <hzycinski@gmail.com>
2022-01-25 18:31:26 +0100

    Add difficulty selection

--- MemoryGame/Program.cs
+++ MemoryGame/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 
+
 namespace MemoryGame
 {
     class Program
@@ -14,12 +15,45 @@ namespace MemoryGame
             catch (FileNotFoundException e)
             {
                 Console.WriteLine($"{e.Message}");
-                Console.WriteLine("Make sure that the Words.txt file is located in the directory given above.");
+                Console.WriteLine("Make sure that the \"Words.txt\" file is located in the directory given above.");
                 Console.ReadKey();
                 return;
             }
 
+            string gameDifficulty = GetDifficulty();
+            Console.WriteLine($"Chosen difficulty: {gameDifficulty}");
+
+
+
+        static string GetDifficulty()
+        {
+            bool chosenDifficulty = false;
+            string difficulty = "";
+
+            Console.Clear();
+            Console.WriteLine("Welcome to Memory Game");
+            Console.WriteLine("Please select a difficulty (Easy or Hard)");
+            // string difficulty = Console.ReadLine().ToLower();
+
+                while (!chosenDifficulty)
+                {
+                    difficulty = Console.ReadLine().ToLower();
+                    if (difficulty != "easy" & difficulty != "hard")
+                    {
+                        Console.WriteLine("Please select one of the available difficulties (Easy or Hard)");
+                        continue;
+                    }
+                    else
+                    {
+                        chosenDifficulty = true;
+                    }
+
+                }
+                return difficulty;
+
+        }
             
+
 
         }
     }
